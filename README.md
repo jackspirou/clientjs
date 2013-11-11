405b9d3e6509e54e41470f08050e1014ff849940
System Administrator <root@Bumblebee.local>
2013-11-10 20:08:28 -0600

    Code compiled and documentation draft 1 completed Nov-10-8pm

--- README.md
+++ README.md
@@ -1,4 +1,128 @@
-fingerprintJS
+ClientJS
 =============
 
-A device fingerprint or machine fingerprint or browser fingerprint is information collected about a remote computing device for the purpose of identification. Fingerprints can fully or partially identify individual users or devices even when cookies are turned off.
+//
+// ClientJS.  An easy to use, simple, and flexible client information library written in JavaScript.
+//
+//      Version: 0.07
+//
+//      Jack Spirou
+//      5 Nov 2013
+
+// ClientJS.  Return a JavaScript object containing information collected about a client.
+//            Return browser/device fingerprint as a 32 bit integer hash ID. 
+
+// BUILT UPON:
+//      - https://github.com/Valve/fingerprintjs
+//      - http://darkwavetech.com/device_fingerprint.html
+//      - detectmobilebrowsers.com JavaScript Mobile Detection Script
+
+// Dependencies Include: 
+//      - ua-parser.js
+//      - fontdetect.js
+//      - swfobject.js
+//      - murmurhash3.js
+
+// BROWSER FINGERPRINT DATA POINTS
+//      - userAgent
+//      - screenPrint
+//          - colordepth
+//          - currentResolution 
+//          - availableResolution
+//          - deviceXDPI
+//          - deviceYDPI
+//      - plugin list
+//      - font list
+//      - localStorage
+//      - sessionStorage
+//      - timezone
+//      - language
+//      - systemLanguage
+//      - cookies
+//      - canvasPrint
+
+// METHOD Naming CONVENTION
+//      is[MethodName]  = return boolean
+//      get[MethodName] = return int|string|object
+
+// METHODS
+//
+//      var client = new ClientJS();
+//
+//      client.getSoftwareVersion();
+//      client.getBrowserData();
+//      client.getFingerPrint();
+//
+//      client.getUserAgent();
+//      client.getUserAgentLowerCase();
+//
+//      client.getBrowser();
+//      client.getBrowserVersion();
+//      client.getBrowserMajorVersion();
+//      client.isIE();
+//      client.isChrome();
+//      client.isFirefox();
+//      client.isSafari();
+//      client.isOpera();
+//
+//      client.getEngine();
+//      client.getEngineVersion();
+//
+//      client.getOS();
+//      client.getOSVersion();
+//      client.isWindows();
+//      client.isMac();
+//      client.isLinux();
+//      client.isUbuntu();
+//      client.isSolaris();
+//
+//      client.getDevice();
+//      client.getDeviceType();
+//      client.getDeviceVendor();
+//
+//      client.getCPU();
+//
+//      client.isMobile();
+//      client.isMobileMajor();
+//      client.isMobileAndroid();
+//      client.isMobileOpera();
+//      client.isMobileWindows();
+//      client.isMobileBlackBerry();
+//
+//      client.isMobileIOS();
+//      client.isIphone();
+//      client.isIpad();
+//      client.isIpod();
+//
+//      client.getScreenPrint();
+//      client.getColorDepth();
+//      client.getCurrentResolution();
+//      client.getAvailableResolution();
+//      client.getDeviceXDPI();
+//      client.getDeviceYDPI();
+//
+//      client.getPlugins();
+//      client.isJava();
+//      client.getJavaVersion();
+//      client.isFlash();
+//      client.getFlashVersion();
+//      client.isSilverlight();
+//      client.getSilverlightVersion();
+//
+//      client.getMimeTypes();
+//      client.isMimeTypes();
+//
+//      client.isFont();
+//      client.getFonts();
+//
+//      client.isLocalStorage();
+//      client.isSessionStorage();
+//      client.isCookie();
+//
+//      client.getTimeZone();
+//
+//      client.getLanguage();
+//      client.getSystemLanguage();
+//
+//      client.isCanvas();
+//      client.getCanvasPrint();
--- src/client.js
+++ src/client.js
@@ -1,13 +1,13 @@
 //
-// ClientJS.  An easy to use and flexible browser information library written in JavaScript.
+// ClientJS.  An easy to use, simple, and flexible client information library written in JavaScript.
 //
 //      Version: 0.07
 //
 //      Jack Spirou
 //      5 Nov 2013
 
-// ClientJS.  Return a JavaScript object containing information collected about a browser.
-//            Return browser fingerprint as a 32 bit integer hash ID. 
+// ClientJS.  Return a JavaScript object containing information collected about a client.
+//            Return browser/device fingerprint as a 32 bit integer hash ID. 
 
 // BUILT UPON:
 //      - https://github.com/Valve/fingerprintjs
@@ -86,7 +86,7 @@
 //      client.isMobileWindows();
 //      client.isMobileBlackBerry();
 //
-//      client.isMobileiOS();
+//      client.isMobileIOS();
 //      client.isIphone();
 //      client.isIpad();
 //      client.isIpod();
@@ -131,7 +131,7 @@
     // Global user agent browser object.
     var browserData;
 
-    // ClientJS constructor which sets the browserData object.
+    // ClientJS constructor which sets the browserData object and returs the client object.
     var ClientJS = function () {
         var parser = new UAParser();
         browserData = parser.getResult();
@@ -145,7 +145,7 @@
         // MAIN METHODS
         //
 
-        // Get Version.  Return a string containing this software version number.
+        // Get Software Version.  Return a string containing this software version number.
         getSoftwareVersion: function () {
             var version = "ClientJS 0.05";
             return version;
@@ -156,8 +156,8 @@
             return browserData;
         },
 
-        // Get Finger Print.  Return an integer representing the browsers fingerprint.
-        getFingerPrint: function () {
+        // Get Fingerprint.  Return a 32-bit integer representing the browsers fingerprint.
+        getFingerprint: function () {
             var bar             = '|';
 
             var userAgent       = browserData.ua;
@@ -328,53 +328,77 @@
             return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(dataString) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(dataString.substr(0, 4)));
         },
 
-        // Is Mobile.  Check if the browser is on a major mobile device.
+        // Is Mobile Major.  Check if the browser is on a major mobile device.
         isMobileMajor: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            return (this.isMobileAndroid() || this.isMobileBlackBerry() || this.isMobileIOS() || this.isMobileOpera() || this.isMobileWindows());
         },
 
         // Is Mobile.  Check if the browser is on an android mobile device.
         isMobileAndroid: function() {
-            return browserData.ua.match(/Android/i);
+            if(browserData.ua.match(/Android/i)) {
+                return true;
+            }
+            return false;
         },
 
-        // Is Mobile.  Check if the browser is on an opera mobile device.
+        // Is Mobile Opera.  Check if the browser is on an opera mobile device.
         isMobileOpera: function() {
-            return browserData.ua.match(/Opera Mini/i);
+            if(browserData.ua.match(/Opera Mini/i)) {
+                return true;
+            }
+            return false;
         },
     
-        // Is Mobile.  Check if the browser is on a windows mobile device.
+        // Is Mobile Windows.  Check if the browser is on a windows mobile device.
         isMobileWindows: function() {
-            return browserData.ua.match(/IEMobile/i);
+            if(browserData.ua.match(/IEMobile/i)) {
+                return true;
+            }
+            return false;
         },
 
-        // Is Mobile.  Check if the browser is on a blackberry mobile device.
+        // Is Mobile BlackBerry.  Check if the browser is on a blackberry mobile device.
         isMobileBlackBerry: function() {
-            return browserData.ua.match(/BlackBerry/i);
+            if(browserData.ua.match(/BlackBerry/i)) {
+                return true;
+            }
+            return false;
         },
 
         //
         // MOBILE APPLE METHODS
         //
 
-        // Is Mobile.  Check if the browser is on an Apple iOS device.
-        isMobileiOS: function() {
-            return browserData.ua.match(/iPhone|iPad|iPod/i);
+        // Is Mobile iOS.  Check if the browser is on an Apple iOS device.
+        isMobileIOS: function() {
+            if(browserData.ua.match(/iPhone|iPad|iPod/i)) {
+                return true;
+            }
+            return false;
         },
 
         // Is Iphone.  Check if the browser is on an Apple iPhone.
         isIphone: function() {
-            return browserData.ua.match(/iPhone/i);
+            if(browserData.ua.match(/iPhone/i)) {
+                return true;
+            }
+            return false;
         },
 
         // Is Ipad.  Check if the browser is on an Apple iPad.
         isIpad: function() {
-            return browserData.ua.match(/iPad/i);
+            if(browserData.ua.match(/iPad/i)) {
+                return true;
+            }
+            return false;
         },
 
         // Is Ipod.  Check if the browser is on an Apple iPod.
         isIpod: function() {
-            return browserData.ua.match(/iPod/i);
+            if(browserData.ua.match(/iPod/i)) {
+                return true;
+            }
+            return false;
         },
 
         //
@@ -603,7 +627,7 @@
             ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
             ctx.fillText(txt, 4, 17);
             return canvas.toDataURL();
-        },
+        }
 
     };
 
