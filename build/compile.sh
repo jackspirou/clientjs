ea92a55c2be9c98bdbd8a22d2c49c1e2a6ada08e
JackSpirou <jack.spirou@me.com>
2015-01-02 01:33:54 -0600

    updating compile script for advanced options

--- build/compile.sh
+++ build/compile.sh
@@ -1,2 +1,2 @@
-java -jar ~/bin/compiler.jar --js=../src/vendor/ua-parser.js --js=../src/vendor/fontdetect.js --js=../src/vendor/swfobject.js --js=../src/vendor/murmurhash3.js --js=../src/vendor/deployJava.js --js=../src/client.js --js_output_file=../src/client.min.js
+java -jar ~/bin/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js=../src/vendor/ua-parser.js --js=../src/vendor/fontdetect.js --js=../src/vendor/swfobject.js --js=../src/vendor/murmurhash3.js --js=../src/vendor/deployJava.js --js=../src/client.js --js_output_file=../src/client.min.js
 cp ../src/client.min.js ../dist/client.min.js
