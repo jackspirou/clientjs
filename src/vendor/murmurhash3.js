a6b5130e40c0bcba6ad9accd69bf8a3f5519b7a6
JackSpirou <jack.spirou@me.com>
2015-01-03 14:40:07 -0600

    updating vendor code

--- src/vendor/murmurhash3.js
+++ src/vendor/murmurhash3.js
@@ -1,15 +1,15 @@
 /**
- * JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
- *
- * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
- * @see http://github.com/garycourt/murmurhash-js
- * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
- * @see http://sites.google.com/site/murmurhash/
- *
- * @param {string} key ASCII only
- * @param {number} seed Positive integer only
- * @return {number} 32-bit positive integer hash
- */
+* JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
+*
+* @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
+* @see http://github.com/garycourt/murmurhash-js
+* @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
+* @see http://sites.google.com/site/murmurhash/
+*
+* @param {string} key ASCII only
+* @param {number} seed Positive integer only
+* @return {number} 32-bit positive integer hash
+*/
 
 function murmurhash3_32_gc(key, seed) {
   var remainder, bytes, h1, h1b, c1, c1b, c2, c2b, k1, i;
@@ -22,11 +22,11 @@ function murmurhash3_32_gc(key, seed) {
   i = 0;
 
   while (i < bytes) {
-      k1 =
-        ((key.charCodeAt(i) & 0xff)) |
-        ((key.charCodeAt(++i) & 0xff) << 8) |
-        ((key.charCodeAt(++i) & 0xff) << 16) |
-        ((key.charCodeAt(++i) & 0xff) << 24);
+    k1 =
+    ((key.charCodeAt(i) & 0xff)) |
+    ((key.charCodeAt(++i) & 0xff) << 8) |
+    ((key.charCodeAt(++i) & 0xff) << 16) |
+    ((key.charCodeAt(++i) & 0xff) << 24);
     ++i;
 
     k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
@@ -34,7 +34,7 @@ function murmurhash3_32_gc(key, seed) {
     k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
 
     h1 ^= k1;
-        h1 = (h1 << 13) | (h1 >>> 19);
+    h1 = (h1 << 13) | (h1 >>> 19);
     h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
     h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
   }
@@ -42,8 +42,8 @@ function murmurhash3_32_gc(key, seed) {
   k1 = 0;
 
   switch (remainder) {
-    case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16; break;
-    case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8; break;
+    case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+    case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
     case 1: k1 ^= (key.charCodeAt(i) & 0xff);
 
     k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
