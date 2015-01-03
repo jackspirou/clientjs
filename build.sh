7b9eb95981ef3337b48e7d8945f2c38ff7f52657
JackSpirou <jack.spirou@me.com>
2015-01-03 12:09:44 -0600

    after experimenting switching to SIMPLE_OPTIMIZATIONS

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@
 
 # Assume compiler.jar exists in the users $HOME/bin directory.
 # Compile using proper settings and include vendor dependiencies.
-java -jar ~/bin/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js=src/client.js --js_output_file=src/client.min.js --externs=src/vendor/ua-parser.js --externs=src/vendor/fontdetect.js --externs=src/vendor/swfobject.js --externs=src/vendor/murmurhash3.js --externs=src/vendor/deployJava.js
+java -jar ~/bin/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js=src/vendor/ua-parser.js --js=src/vendor/fontdetect.js --js=src/vendor/swfobject.js --js=src/vendor/murmurhash3.js --js=src/vendor/deployJava.js --js=src/client.js --js_output_file=src/client.min.js
 
 # Copy the compiled source file to the dist (distribution) directory.
 cp src/client.min.js dist/client.min.js
