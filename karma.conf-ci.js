c9077dd92c00c6b83643c417a0d938d7c3492953
jackspirou <jack.spirou@target.com>
2016-01-23 14:57:26 -0600

    adding more chrome platforms and devices for testing

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -15,10 +15,150 @@ module.exports = function(config) {
 
   // Browsers to run on Sauce Labs
   var customLaunchers = {
+  ////
+  // Chrome
+  ////
+
+    ////
+    // Windows OS
+    ////
     'SL_Chrome': {
       base: 'SauceLabs',
-      browserName: 'chrome'
+      browserName: 'chrome',
+      platform: 'Windows XP',
     },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 8',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 8.1',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 10',
+    },
+
+    ////
+    // Mac OSX
+    ////
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X Mountain Lion 10.8',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X Mavericks 10.9',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X Yosemite 10.10',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X El Capitan 10.11',
+    },
+
+    ////
+    // Mac OSX
+    ////
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X Mountain Lion 10.8',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X Mavericks 10.9',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X Yosemite 10.10',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X El Capitan 10.11',
+    },
+
+    ////
+    // Linux
+    ////
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Linux',
+    },
+
+    ////
+    // Apple iOS
+    ////
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'iOS Simulator',
+    },
+
+    ////
+    // Google Android
+    ////
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 2.3',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 4.0',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 4.1',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 4.2',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 4.3',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 4.4',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 5.0',
+    },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Android 5.1',
+    },
+
+
     'SL_InternetExplorer': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
@@ -27,8 +167,28 @@ module.exports = function(config) {
     'SL_FireFox': {
       base: 'SauceLabs',
       browserName: 'firefox',
+    },
+    'SL_Edge': {
+      base: 'SauceLabs',
+      platform: 'Windows 10',
+      browserName: 'microsoftedge'
     }
   };
+
+  'SL_Chrome': {
+      base: 'SauceLabs',
+      platform: 'OS X 10.11',
+      browserName: 'chrome',
+      customData: {
+        awesome: true
+      }
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      platform: 'OS X 10.11',
+      browserName: 'firefox'
+    },
+  }
 
   config.set({
 
