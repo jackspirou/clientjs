1b609b184e2b7fa86324592faa419c86cf3593ce
jackspirou <jack.spirou@target.com>
2016-01-24 11:20:40 -0600

    increasing timeout limit for sauce to find a test machine

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -91,7 +91,7 @@ module.exports = function(config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    captureTimeout: 120000,
+    captureTimeout: 180000,
     customLaunchers: customLaunchers,
 
     // start these browsers
