d58557b5478faeec4b8a2ef154ed214bc73330b8
jackspirou <jack.spirou@target.com>
2016-01-24 15:13:34 -0600

    going back to log info and removing jasmine-core dep

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -94,7 +94,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_ERROR,
+    logLevel: config.LOG_INFO,
 
     captureTimeout: 180000,
     customLaunchers: customLaunchers,
